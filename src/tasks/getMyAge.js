76630e1948f5f1b78a9322c58f8cc4df2ca4e7ca
Kamilnr10 <kamil.nr10@gmail.com>
2020-09-16 17:41:45 +0200

    Task 1 with typescript - to check

--- src/tasks/getMyAge.js
+++ src/tasks/getMyAge.js
@@ -1,6 +1,9 @@
 var getMyAge = function (input) {
     var date = new Date();
     var actualYear = date.getFullYear();
+    if (!input) {
+        throw new Error("Input cannot be empty");
+    }
     if (input > actualYear) {
         throw new Error("Input should be less or equal to actual year");
     }
--- src/tasks/getMyAge.ts
+++ src/tasks/getMyAge.ts
@@ -1,6 +1,9 @@
-const getMyAge = (input: number) => {
+const getMyAge = (input: any): number => {
   const date = new Date();
   const actualYear = date.getFullYear();
+  if (!input) {
+    throw new Error(`Input cannot be empty`);
+  }
   if (input > actualYear) {
     throw new Error(`Input should be less or equal to actual year`);
   }
