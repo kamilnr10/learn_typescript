28ed372e2ea9d9ba835c684235cf88c40fb670f6
Kamilnr10 <kamil.nr10@gmail.com>
2020-09-22 21:48:17 +0200

    Working on arrayMethods

--- src/tasks/arrayMethod.ts
+++ src/tasks/arrayMethod.ts
@@ -1,6 +1,9 @@
+// Stwórz funkcje, które będą działać identycznie co metody wbudowane
+// ale będą działać przy pomocy pętli for lub while
+
 const arrayOfNumbers = [2, 3, 12, 53, 8, 1];
 
-function mapFn(array: any[], callback) {
+function mapFn(array: any[], callback: () => void) {
   let mapArray = [];
   for (let i = 0; i < array.length; i++) {
     const result = callback(array[i], i, array);
@@ -9,12 +12,53 @@ function mapFn(array: any[], callback) {
   return mapArray;
 }
 
-function filterFn(array, callback) {}
+function filterFn(array, callback) {
+  const filterArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = callback(array[i], i, array);
+    if (result) filterArr.push(arr[i]);
+  }
+  return filterArr;
+}
 
-function reduceFn(array, callback, initial) {}
+function reduceFn(array, callback, initial) {
+  let acc = initial === undefined ? 0 : initial;
+  for (let i = 0; i < array.length; i++) {
+    acc = callback(acc, array[i], i, array);
+    return acc;
+  }
+}
 
 function reduceRightFn(array, callback, initial) {}
 
-function everyFn(array, callback) {}
+const additionAllElements = (a, b) => {
+  return a + b;
+};
 
-function someFn(array, callback) {}
+function everyFn(array, callback) {
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    if (!callback(value, index, array)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function someFn(array, callback) {
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    if (callback(value, index, array)) {
+      return true;
+    }
+  }
+
+  return false;
+}
--- src/tasks/arrayMethods.js
+++ src/tasks/arrayMethods.js
@@ -12,12 +12,63 @@ function mapFn(array, callback) {
   return mapArray;
 }
 
-function filterFn(array, callback) {}
+function filterFn(array, callback) {
+  const filterArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = callback(array[i], i, array);
+    if (result) filterArr.push(arr[i]);
+  }
+  return filterArr;
+}
 
-function reduceFn(array, callback, initial) {}
+function reduceFn(array, callback, initial) {
+  let acc = initial === undefined ? 0 : initial;
+  for (let i = 0; i < array.length; i++) {
+    acc = callback(acc, array[i], i, array);
+    return acc;
+  }
+}
 
-function reduceRightFn(array, callback, initial) {}
+function reduceRightFn(array, callback, initial) {
+  let acc = initial === undefined ? 0 : initial;
+  for (let i = array.length - 1; i < array.length; i--) {
+    acc = callback(acc, array[i], i, array);
+    return acc;
+  }
+}
 
-function everyFn(array, callback) {}
+const additionAllElements = (a, b) => {
+  return a + b;
+};
 
-function someFn(array, callback) {}
+const arrayOfNumbers = [2, 4, 5, 7];
+
+console.log(reduceRightFn(arrayOfNumbers, additionAllElements));
+
+function everyFn(array, callback) {
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    if (!callback(value, index, array)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function someFn(array, callback) {
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    if (callback(value, index, array)) {
+      return true;
+    }
+  }
+
+  return false;
+}
