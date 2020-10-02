603a30db56fe9f6867e935cf808bab0199e61fef
Kamilnr10 <kamil.nr10@gmail.com>
2020-10-02 20:57:37 +0200

    task findWordInArray is done

--- src/tasks/findWordInArray.ts
+++ src/tasks/findWordInArray.ts
@@ -6,44 +6,21 @@ const inputData = ["stwórz", "sobie", "tutaj", "więcej", "wyrazów"];
 
 // generyczny typ
 //
-function findPhraseInArray(array: string[], word: string): string {
-  // brak walidacji - czy word nie jest pusty
-
-  // includes jest słabe - case sensitive
-  // w jednej pętli -.splice i .findIndex
-  // wyszukiwanie w stringu przez regexp
-
-  if (array.includes(word)) {
-    return `Word: ${word} is in Array. Index of this word is: ${array.indexOf(
-      word
-    )}`;
-  } else {
-    return `There is no word in Array`;
-  }
+type tuple<T> = [T, number];
+function searchPhrase<T>(array: T, phrase: string): Array<tuple<T>> {
+  const phraseLowerCase = phrase.toLowerCase();
+  const initialValue: Array<tuple<T>> = [];
+  return array.reduce(function (accum, currValue, index, arr) {
+    const currentValueLowerCase = currValue.toLowerCase();
+    if (currentValueLowerCase.indexOf(phraseLowerCase) !== -1) {
+      accum.push([index, currValue]);
+    }
+    return accum;
+  }, initialValue);
 }
 
-type arrayOfWords = Array<string>;
-
-function findWord(array: arrayOfWords, phrase: string) {
-  if (!array.length) {
-    throw new Error("Array cant be empty");
-  }
-
-  if (phrase.length < 3) {
-    throw new Error("Second argument should be at least 3 char length");
-  }
-
-  // find index by .indexOf albo regexp
-  // .reduce
-  const searchWordInArray = array.find((item) => {
-    if (phrase === item) return true;
-    else return false;
-  });
-
-  const searchIndexOfWord = array.findIndex(
-    (item) => item.toLocaleLowerCase().indexOf(phrase) > -1
-  );
-  return `Index of ${searchWordInArray} is ${searchIndexOfWord}`;
-}
-
-console.log(findPhraseInArray(inputData, "tomek"));
+var sum = inputData.reduce(function (x, y, i, ar) {
+  console.log("x: " + x + ", y: " + y + ", i: " + i + ", sum: " + (x + y));
+  return x + y;
+}, 0);
+console.log(searchPhrase(inputData, "tomek"));
